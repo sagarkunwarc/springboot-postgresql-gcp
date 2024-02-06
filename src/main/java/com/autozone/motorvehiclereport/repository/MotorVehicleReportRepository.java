58cb60c440533dc73848ed2c4fc398b07d02df5c
skunwarc <sagar.kunwar24@gmail.com>
2024-02-06 10:23:43 -0500

    code refactor

--- src/main/java/com/autozone/motorvehiclereport/repository/MotorVehicleReportRepository.java
+++ src/main/java/com/autozone/motorvehiclereport/repository/MotorVehicleReportRepository.java
@@ -2,8 +2,8 @@ package com.autozone.motorvehiclereport.repository;
 
 import com.autozone.motorvehiclereport.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-public interface MotorVehicleReportRepository extends CrudRepository<Vehicle,Long> {
+@Repository
+public interface MotorVehicleReportRepository extends JpaRepository<Vehicle,Long> {
 }
